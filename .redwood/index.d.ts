409829380984187413272e1ca52bf3057a2e7a2e
Dale French <dalef@fmi.co.za>
2020-10-12 18:52:15 +0200

    chore: update redwood on api side

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2020-10-12T14:54:29.503Z
+// 2020-10-12T16:43:37.754Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
